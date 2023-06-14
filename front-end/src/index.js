2f39684d91e1f614c9eaf58846bb66dd7dd2b6eb
Barbara Oliveira <94144100+barbaraoliveira2308@users.noreply.github.com>
2023-06-14 18:28:13 +0100

    cards

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MyApp from './App';
+import Card from './components/Card';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <MyApp />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-
